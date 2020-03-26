16aca20cccbbd068626e14cbe53273da0fec10dc
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-03-26 22:11:34 +0100

    teacher ranking 6

--- dreamteam-ekorepetycje-console-app/src/main/java/com/infoshareacademy/calendar/CalendarFileReader.java
+++ dreamteam-ekorepetycje-console-app/src/main/java/com/infoshareacademy/calendar/CalendarFileReader.java
@@ -1,10 +1,13 @@
 package com.infoshareacademy.calendar;
 
 
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Calendar;
 
 public class CalendarFileReader {
 
