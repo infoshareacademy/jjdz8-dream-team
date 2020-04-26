7e62df35508d30d6d2877433e1d1c6746e450ed0
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-04-26 14:45:53 +0200

    first page

---
+++ zad1/src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.of(2020,02,02);
+        SimpleFormatter simpleFormatter = 
+        System.out.println(localDate.
+}
