fe50ee4d6c9fcde54f363fd9742408dbd52d7609
Katarzyna <klagocka@gmail.com>
2020-05-31 12:38:43 +0200

    Added javafx graphs

---
+++ webapp/src/main/java/com/infoshareacademy/Wykresy/MedianaWykres.java
@@ -0,0 +1,83 @@
+package com.infoshareacademy.Wykresy;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+
+public class MedianaWykres extends Application {
+    public static long median(double[]a)
+    {
+        Arrays.sort(a);
+        int middle = a.length / 2;
+        try{
+            if (a.length % 2 == 0)
+
+            {
+                double left = a[middle - 1];
+                double right = a[middle];
+
+                return (long) ((left + right) / 2);}
+
+            else{
+                return (long) a[middle];}}
+        catch (ArrayIndexOutOfBoundsException e){
+            return 0;
+        }}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+
+
+        String Nick1 = "Kasia";
+        String Nick2 = "Basia";
+        String Nick3 = "Konrad";
+        String Nick4= "Antek";
+        double   maxiNumbers1[]=new double[]{5.0,4.0,6.0,2.5,3.5} ;
+        double   maxiNumbers2[]= new double[]{5.0,6.0,6.0,2.5,3.5};
+        double maxiNumbers3[]=new double[]{4.0,4.0,4.0,4.0,4.0,4.0};
+        double maxiNumbers4[]=new double[]{2.0,2.0,2.0,2.0,2.0,2.0};
+        double maxiNumbers5[]=new double[]{2.5,2.5,2.5};
+        //Configuring category and NumberAxis
+        CategoryAxis xaxis= new CategoryAxis();
+        NumberAxis yaxis = new NumberAxis(1,12,1);
+        xaxis.setLabel("Mediana");
+        yaxis.setLabel("Mediana");
+        //Configuring BarChart
+        BarChart<String,Float> bar = new BarChart(xaxis,yaxis);
+        bar.setTitle("Mediana");
+        double maxAll=median(maxiNumbers1);
+        double maxAll2=median(maxiNumbers2);
+        double maxAll3=median(maxiNumbers3);
+        double maxAll4=median(maxiNumbers4);
+
+        XYChart.Series<String,Float> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data(Nick1,maxAll));
+
+        series.getData().add(new XYChart.Data(Nick2,maxAll2));
+        series.getData().add(new XYChart.Data(Nick3,maxAll3));
+        series.getData().add(new XYChart.Data(Nick4,maxAll4));
+
+
+        bar.getData().add(series);
+
+
+        Group root = new Group();
+        root.getChildren().add(bar);
+        Scene scene = new Scene(root,600,400);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Mediana");
+        primaryStage.show();
+        scene.getStylesheets().add(getClass().getResource("chart4.css").toExternalForm());
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }}
---
+++ webapp/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module javafx{
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    //opens org.openjfx to javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires java.logging;
+    requires javafx.swing;
+    exports com.infoshareacademy.Wykresy;
+
+}
